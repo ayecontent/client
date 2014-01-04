aae185d0bb780bad2f2c89cf5bc4232caf94ab83
phoerix <phoerix@gmail.com>
2014-01-04 15:20:31 +0300

    AYECM-23: Implement the client prototype

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-module.exports = require("./lib/client.js");
+//module.exports = require("./lib/client.js");
+
+var forever = require('forever-monitor');
+
+ var child = new (forever.Monitor)('./lib/client.js', {
+   max: 10,
+   silent: false,
+   options: []
+ });
+
+ child.on('exit', function () {
+   console.log('client.js has exited after 3 restarts');
+ });
+
+ child.start();
